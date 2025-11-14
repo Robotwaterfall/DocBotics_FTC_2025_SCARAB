6ba3625270c6f2d0a02002dfae1119e612e5b2d9
Robotwaterfall <jaysonwelshman@gmail.com>
2025-11-13 22:29:42 -0500

    the robot can now successfully launch one artificat.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -16,8 +16,8 @@ public class Constants {
     public static final int TICKS_PER_OUTPUT_REV = (int)(MOTOR_TICKS_PER_REV * CATA_GEAR_REDUCTION);
     public static final double DEGREES_PER_TICK = 360.0 / TICKS_PER_OUTPUT_REV;
 
-    public static int cata_Up_setpoint = 6;
-    public static int cata_Down_setpoint = -6;
+    public static int cata_Up_setpoint = 5;
+    public static int cata_Down_setpoint = -5;
 
     public static double RUBBER_BAND_FEEDFORWARD = 0.2; // Start low, tune higher if needed
 
